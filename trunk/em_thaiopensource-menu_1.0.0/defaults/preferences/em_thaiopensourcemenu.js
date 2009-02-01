5d6de5830a41f38ed42e2d1ac8fb8b285a6edd7c
anoochit <anoochit@a29dc85a-ced0-11dd-a405-31afbefa9e0e>
2009-02-01 03:21:59 +0000

    add 1st release

---
+++ trunk/em_thaiopensource-menu_1.0.0/defaults/preferences/em_thaiopensourcemenu.js
@@ -0,0 +1,29 @@
+/*
+ This file was automagically generated on 2009-02-01 03:13:18 UTC
+ by Easy Menu v. 1.0.3 ( http://easymenu.googlepages.com/ )
+ and is part of 'Thai Open Source'.
+
+ Thai Open Source is free software: you can redistribute it
+ and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version.
+
+ Thai Open Source is distributed in the hope that it will be
+ useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Thai Open Source.
+ If not, see http://www.gnu.org/licenses/.
+
+ The copy of the GNU General Public License can be found here:
+ chrome://em_thaiopensourcemenu/content/GPLv3License.txt
+*/
+
+pref("extensions.{84f12230-d3b4-4fd2-8e57-077f94ea76eb}.description","chrome://em_thaiopensourcemenu/locale/em_thaiopensourcemenu.properties");
+pref("extensions.em_thaiopensourcemenu.TitleMenu", true);
+pref("extensions.em_thaiopensourcemenu.ToolsMenu", true);
+pref("extensions.em_thaiopensourcemenu.ContextMenu", true);
+pref("extensions.em_thaiopensourcemenu.Options", true);
+pref("extensions.em_thaiopensourcemenu.ShowLogo", true);
